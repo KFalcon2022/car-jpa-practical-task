80d3710fca273fb13b4935cb0b9b623b37ae5ae9
ааа <@d.d>
2025-04-10 01:48:22 +0400

    Small fix

--- src/main/java/com/walking/carpractice/domain/Car.java
+++ src/main/java/com/walking/carpractice/domain/Car.java
@@ -37,7 +37,7 @@ public class Car {
     @ManyToMany(mappedBy = "cars", fetch = FetchType.EAGER)
     private List<User> owners = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "fk_model")
     private Model model;
 
